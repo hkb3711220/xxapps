88a590b3a8298b31d391079bc299fb45dced3a7a
CHANGLI HUANG <chanhu@CHANGLIdeMacBook-Air.local>
2022-08-31 22:09:40 +0900

    refactor source code

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ from authlib.integrations.flask_client import OAuth
 from urllib import parse
 import logging
 import os
+from authlib.integrations.requests_client import OAuth2Session
 
 os.environ['AUTHLIB_INSECURE_TRANSPORT'] = '1' #redirect to http not https 
 
@@ -12,7 +13,7 @@ logger = logging.getLogger(__name__)
 app = Flask(__name__)
 app.secret_key = '!secret'
 #keycloak関連を定義
-keycloak_host = "172.17.0.2:8080" #環境変数
+keycloak_host = "172.18.0.2:8080" #環境変数
 keycloak_relm_id = 'demo' #環境変数
 oauth = OAuth(app)
 oauth.register(
@@ -34,10 +35,12 @@ oauth.register(
 def index():
     user = dict(session).get('user', None)
     token = dict(session).get('token', None)
+
     if user:
         return render_template("home.html", user=user, token=token)
     else:
         return render_template("login.html")
+
 
 @app.route('/login')
 def login():
@@ -53,7 +56,6 @@ def auth():
     resp.raise_for_status()
     profile = resp.json()
     session['token'] = token
-    session['id_token'] = token['id_token']
     session['user'] = profile
 
     return redirect('/')
@@ -61,9 +63,9 @@ def auth():
 @app.route('/logout')
 def logout():
     
-    id_token_hint = session.get('id_token', None)
+    token = session.get('token', None)
+    id_token_hint = token['id_token']
     session.pop('user', None)
-    session.pop('id_token', None)
     session.pop('token', None)
     #redirect先で/と別のとこに遷移したい場合、以下のように設定を追加
     #&post_logout_redirect_uri='+parse.quote('リダイレクト先URL')
